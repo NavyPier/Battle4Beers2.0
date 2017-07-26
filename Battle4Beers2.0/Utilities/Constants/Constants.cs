a9047af7068a99f901fd576abc607c66d457e7da
Veselin Uzunov <vega_99@abv.bg>
2017-07-26 14:37:25 +0300

    Edit Constants

--- Battle4Beers2.0/Utilities/Constants/Constants.cs
+++ Battle4Beers2.0/Utilities/Constants/Constants.cs
@@ -1,5 +1,4 @@
-﻿<<<<<<< .mine
-namespace Battle4Beers2._0.Constants
+﻿namespace Battle4Beers2._0.Constants
 {
     public class Constants
     {
@@ -20,26 +19,3 @@ namespace Battle4Beers2._0.Constants
           |____________|         |___| |____________|";
     }
 }
-=======
-namespace Battle4Beers2._0.Constants
-{
-    public class Constants
-    {
-        //Error messages.
-        public const string InvalidPlayerSymbolsCount = @"- A player's name must be longer than 3 symbols!!!
-                                                        - A player's name must be shorter than 16 symbols!!!";
-
-        //Drawing constants
-        public const string GameTitle = @"          _________       ___     ___  _________
-          |   ___  |     |   |   |   | |   ___  |
-          |  |___| |     |   |   |   | |  |___| |
-          |________|__   |   |___|   | |________|__
-          |  ______    | |_______    | |  ______    |
-          |  |     |   |         |   | |  |     |   |
-          |  |_____|   |         |   | |  |_____|   |
-          |____________|         |___| |____________|";
-    }
-}
-
-
->>>>>>> .theirs
