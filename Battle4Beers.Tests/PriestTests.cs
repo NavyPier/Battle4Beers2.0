b35a4a6b121797fffbcde8ba41b57da9520323ef
AlexJC <ajc@abv.bg>
2017-08-14 20:03:26 +0300

    Added Priest Test

--- Battle4Beers.Tests/PriestTests.cs
+++ Battle4Beers.Tests/PriestTests.cs
@@ -1,32 +1,32 @@
-﻿using Battle4Beers.Client.Models;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System.Collections.Generic;
+using Battle4Beers.Client.Models;
 using Action = Battle4Beers.Client.Models.Action;
 
 namespace Battle4Beers.Tests
 {
     [TestFixture]
-    public class MageTests
+    public class PriestTests
     {
         private const int health = 20;
-        private const int fullHealth = 2200;
+        private const int fullHealth = 2100;
         private const int healthRegeneration = 50;
         private const int armor = 5;
         private const int mana = 20;
-        private const int fullMana = 2500;
+        private const int fullMana = 2700;
         private const int spellPower = 10;
         private const int manaRegen = 100;
 
-        private Mage sut;
+        private Priest sut;
 
         [SetUp]
         public void TestInit()
         {
-            sut = new ArcaneMage("Erystrazsa", health, healthRegeneration, new List<Action>(), armor, mana, manaRegen, spellPower);
+            sut = new HolyPriest("Patriarch", health, healthRegeneration, new List<Action>(), armor, mana, manaRegen, spellPower);
         }
 
         [Test]
-        public void IsMageHeroRegenerate()
+        public void IsPriestHeroRegenerate()
         {
             //Arrange
 
@@ -34,20 +34,20 @@ namespace Battle4Beers.Tests
             sut.Regenerate();
             //Assert
             Assert.AreEqual(70, sut.Health);
-            Assert.AreEqual(120, sut.Mana);
+            Assert.AreEqual(130, sut.Mana);
         }
 
         [Test]
-        public void IsMageHeroDoesNotRegenerateWhenHealthAndManaAreFull()
+        public void IsPriestHeroDoesNotRegenerateWhenHealthAndManaAreFull()
         {
             //Arrange
-            this.sut = new ArcaneMage("Erystrazsa", fullHealth, healthRegeneration, new List<Action>(), armor, fullMana, manaRegen, spellPower );
+            sut = new HolyPriest("Patriarch", fullHealth, healthRegeneration, new List<Action>(), armor, fullMana, manaRegen, spellPower);
 
             //Act
             sut.Regenerate();
             //Assert
-            Assert.AreEqual(2200, sut.Health);
-            Assert.AreEqual(2500, sut.Mana);
+            Assert.AreEqual(2100, sut.Health);
+            Assert.AreEqual(2700, sut.Mana);
         }
     }
 }
