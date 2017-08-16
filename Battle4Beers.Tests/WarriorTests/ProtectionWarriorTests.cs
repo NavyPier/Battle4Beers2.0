a345d84d2e71885235d768490b668b5b81500718
AlexJC <ajc@abv.bg>
2017-08-17 00:31:52 +0300

    Protection Warrior Tests Add

---
+++ Battle4Beers.Tests/WarriorTests/ProtectionWarriorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Battle4Beers.Client.Models;
+using Battle4Beers.Client.Models.Actions.WarriorActions;
+
+
+namespace Battle4Beers.Tests
+{
+    [TestFixture]
+    public class ProtectionWarriorTests
+    {
+        private const int health = 20;
+        private const int healthRegeneration = 50;
+        private const int armor = 5;
+        private const int rage = 20;
+        private const int attackPower = 10;
+
+        [Test]
+        public void ActivatePassiveWithHibernate()
+        {
+            //Arrange
+            ProtectionWarrior sut = new ProtectionWarrior("Protector", health, healthRegeneration, new List<Action>() { new Hibernate("HIBERNATE", 3, 3, 4) }, armor, rage, attackPower);
+
+            //Act
+            sut.ActivatePassive("HIBERNATE", sut);
+
+            //Assert
+            Assert.IsTrue(sut.Hibernating);
+            Assert.AreEqual(1, sut.PassiveDuration);
+        }
+    }
+}
